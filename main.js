9d9c2b72502b3176edbe1af52a64d026b502f351
guixinchan <chan249@foxmail.com>
2021-09-22 09:54:33 +0800

    完善代码

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ function getDefaultHeaders(cookies) {
 /**
  * 自动学习青年大学习
  *
- * @param members 团员账号列表
+ * @param member
  */
 async function autoLearning(member) {
     const cookiesAndValidationCode = await getCookiesAndValidationCode();
@@ -118,7 +118,7 @@ function getIncompleteMembers(secretary) {
             setTimeout(() => {
                 /* 获取当前最新的季度号 */
                 axios.post('https://m.fjcyl.com/admin/cylOrgMembers/groupByList?groupBy=1', null, getDefaultAxiosConfig(object.cookies)).then(async resp => {
-                    const quarterNo = resp?.data?.rs?.[0]?.quarterNo;
+                    const quarterNo = resp?.data?.['rs']?.[0]?.['quarterNo'];
 
                     if (!quarterNo) {
                         console.log("获取当前季度号 `quarterNo` 失败");
@@ -126,7 +126,7 @@ function getIncompleteMembers(secretary) {
                     }
 
                     const {data: userSession} = await axios.post('https://m.fjcyl.com/getUserSession', null, getDefaultAxiosConfig(object.cookies));
-                    const organizationId = await userSession?.rs?.currentCylOrgMember?.orgId;
+                    const organizationId = await userSession?.['rs']?.['currentCylOrgMember']?.['orgId'];
 
                     if (!organizationId) {
                         console.log("未成功获取个人信息中的组织号 orgId");
@@ -135,16 +135,16 @@ function getIncompleteMembers(secretary) {
                     // 获取最新一期的 group id
                     axios.post(`https://m.fjcyl.com/admin/cylOrgMembers/selectList?orderBy=1&quarterNo=${quarterNo}`, null, getDefaultAxiosConfig(object.cookies))
                         .then(resp => {
-                            const groupStudyId = resp?.data?.rs[0]?.guoupStudyId;
+                            const groupStudyId = resp?.data?.['rs']?.[0]?.['guoupStudyId'];
                             if (groupStudyId) {
                                 // 查询学习情况列表
                                 axios.post(`https://m.fjcyl.com/admin/cylOrgMembers/selectCurrentStudy?` +
                                     `studyId=${groupStudyId}&current=&PAGE_SIZE=40&CURRENT_PAGE=1&orgId=${organizationId}`,
                                     null, getDefaultAxiosConfig(object.cookies)).then((resp) => {
-                                    const groups = _.groupBy(resp.data.rs.rs, o => o.isStudy === '否');
+                                    const groups = _.groupBy(resp.data?.['rs']?.['rs'], o => o['isStudy'] === '否');
                                     const data = {
-                                        "incomplete": _.map(groups.true, 'acctName'),
-                                        "complete": _.map(groups.false, 'acctName'),
+                                        "incomplete": _.map(groups['true'], 'acctName'),
+                                        "complete": _.map(groups['false'], 'acctName'),
                                     }
 
                                     let imageMagick = gm.subClass({imageMagick: true});
@@ -155,7 +155,7 @@ function getIncompleteMembers(secretary) {
 
                                     let x = 260;
                                     let y = 150;
-                                    const LENGTH = 80;
+                                    // const LENGTH = 80;
                                     let length = 1200 / data.incomplete.length;
                                     incompleteImage.drawText(130, 80, '青年大学习未完成名单')
                                     for (let name of data.incomplete) {
